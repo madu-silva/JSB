5e349b2014b22207da443cfe3adb465d1e37f9ca
Maria Eduarda <mariaeduarda050501@gmail.com>
2024-09-29 09:35:16 -0300

    correcao ex07

--- lista01/ex07/js/scripts.js
+++ lista01/ex07/js/scripts.js
@@ -1,17 +1,13 @@
 var r = prompt("Informe um valor entre 0 e 255 para o primeiro valor do RGB:");
-
 while ((r < 0) || (r > 255) || isNaN(r)) 
     r = prompt("Informe um valor entre 0 e 255:");
 
 var g = prompt("Informe um valor entre 0 e 255 para o segundo valor do RGB:");
-
 while ((g < 0) || (g > 255) || isNaN(g)) 
     g = prompt("Informe um valor entre 0 e 255:");
 
 var b = prompt("Informe um valor entre 0 e 255 para o terceiro valor do RGB:");
-
 while ((b < 0) || (b > 255) || isNaN(b)) 
-    rbg3 = prompt("Informe um valor entre 0 e 255:");
+    b = prompt("Informe um valor entre 0 e 255:");
 
 document.write(`<p style="color:rgb(${r}, ${g}, ${b})">Fundamentos de Web Design II</p>`)
-
