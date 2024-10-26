9f69b7345e165710d75c2984cd70709a0056c519
Maria Eduarda <mariaeduarda050501@gmail.com>
2024-10-25 23:32:22 -0300

    melhoria ex8

--- lista04/ex08/js/script.js
+++ lista04/ex08/js/script.js
@@ -1,20 +1,20 @@
 // 1º solução
-const matriz = [
+const caracteres = [
     ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
     ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
     ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 ];
 
-function gerarSenha(tamanho) {
-    let senha = '';
+function gerarSenha(tamanhoSenha) {
+    let senha = [];
 
-    for (let i = 0; i < tamanho; i++) {
-        const linha = Math.floor(Math.random() * matriz.length);
-        const coluna = Math.floor(Math.random() * matriz[linha].length);
-        senha += matriz[linha][coluna];
+    for (let i = 0; i < tamanhoSenha; i++) {
+        const linha = Math.floor(Math.random() * caracteres.length);
+        const coluna = Math.floor(Math.random() * caracteres[linha].length);
+        senha.push(caracteres[linha][coluna]);
     }
 
-    return senha;
+    return senha.join('');
 }
 
 const senhaForte = gerarSenha(8);
